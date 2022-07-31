1569b52895d5dd10f98c25ef218f93d2307e3bf8
Lokesh Parab <98585144+lokeshparab454545@users.noreply.github.com>
2022-07-31 12:33:14 +0530

    Time: 723 ms (44.91%), Space: 150.7 MB (88.22%) - LeetHub

---
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -0,0 +1,53 @@
+class NumArray {
+public:
+        vector<int> _nums;
+        vector<int> bit;
+        
+        int lower_bit(int i){
+            return i&-i;
+        }
+        
+        int query(int i){
+            i++;
+            int sum=0;
+            while(i>0){
+                sum+=bit[i];
+                i-=lower_bit(i);
+            }
+            return sum;
+        }
+        
+        void add(int i, int val){
+            i++;
+            while(i<bit.size()){
+                bit[i]+=val;
+                i+=lower_bit(i);
+            }
+        }
+        
+    public:
+        NumArray(vector<int> &nums) : _nums(nums) {
+            bit.resize(nums.size()+1);
+            for(int i=0; i<nums.size(); i++){
+                add(i, nums[i]);
+            }
+        }
+    
+        void update(int i, int val) {
+            if(val!=_nums[i]){
+                add(i, val-_nums[i]);
+                _nums[i]=val;
+            }
+        }
+    
+        int sumRange(int i, int j) {
+            return query(j)-query(i-1);
+        }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
