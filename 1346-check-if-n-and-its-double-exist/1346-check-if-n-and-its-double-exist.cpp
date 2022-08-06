618efb35ae238d46d0079628a8bd8ccfd4b78d07
Lokesh Parab <98585144+lokeshparab454545@users.noreply.github.com>
2022-08-06 12:10:02 +0530

    Time: 20 ms (14.46%), Space: 11.2 MB (13.98%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
-    bool binS(vector<int>&arr, int tar, int check){
-        int n = arr.size();
-        int s = 0, e = n-1;
-        while(s<=e){
-            int m = s + (e-s)/2;
-            if(arr[m] == tar && m != check){
-                return true;
-            }
-            else if(arr[m] < tar){
-                s = m+1;
-            }
-            else{
-                e = m-1;
-            }
-        }
-        return false;
-    }
     bool checkIfExist(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
+        unordered_map<int, int>mp;
         for(int i = 0; i<arr.size(); i++){
-            int tar = 2 * arr[i];
-            if(binS(arr, tar, i)){
+            mp[arr[i]] = i;
+        }
+        for(int i = 0; i<arr.size(); i++){
+            if(mp.find(arr[i]*2) != mp.end() && mp[arr[i]*2] != i){
                 return true;
             }
         }
