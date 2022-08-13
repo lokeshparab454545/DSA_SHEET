0d9bd9ffabffaad4dbf53167aa1d432668e18576
Lokesh Parab <98585144+lokeshparab454545@users.noreply.github.com>
2022-08-13 11:41:29 +0530

    Time: 24 ms (36.11%), Space: 15.5 MB (75.51%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
-    int binS(vector<int>&numbers, int n, int tar, int i){
-        int s = 0, e = n-1;
-        while(s<=e){
-            int m = s + (e-s)/2;
-            if(numbers[m] ==  tar && m != i){
-                return m;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int>ans;
+        int i = 0, j = n-1;
+        while(i<j){
+            int sum = nums[i] + nums[j];
+            if(sum == target){
+                ans.push_back(i+1);
+                ans.push_back(j+1);
+                i++; j--;
             }
-            else if(numbers[m] > tar){
-                e = m-1;
+            else if(sum > target){
+                j--;
             }
             else{
-                s = m+1;
-            }
-        }
-        return -1;
-    }
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int n = numbers.size();
-        vector<int>ans;
-        for(int i=0; i<n; i++){
-            int tar = target - numbers[i];
-            if(binS(numbers, n, tar, i) != -1){
-                int temp = binS(numbers, n, tar, i);
-                ans.push_back(i+1);
-                ans.push_back(temp+1);
-                break;
+                i++;
             }
         }
         return ans;
