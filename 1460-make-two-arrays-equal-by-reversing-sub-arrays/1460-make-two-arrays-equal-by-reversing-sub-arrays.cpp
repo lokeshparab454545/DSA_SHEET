aff92fe4dfeed57c7bcab70501191c0253ed6f28
Lokesh Parab <98585144+lokeshparab454545@users.noreply.github.com>
2022-08-16 14:53:11 +0530

    Time: 26 ms (32.59%), Space: 14.3 MB (33.74%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        // sort(target.begin(), target.end());
-        // sort(arr.begin(), arr.end());
-        // if(target.size() != arr.size()) return false;
-        // int i = 0, j = 0;
-        // while(i<target.size()){
-        //     if(target[i] != arr[j]){
-        //         return false;
-        //     }
-        //     i++; j++;
-        // }
-        // return true;
-        
-        unordered_map<int, int>mp;
-        for(auto it: target){
-            mp[it]++;
-        }
-        for(auto it: arr){
-            mp[it]--;
-        }
-        for(auto it: mp){
-            if(it.second != 0){
+        sort(target.begin(), target.end());
+        sort(arr.begin(), arr.end());
+        if(target.size() != arr.size()) return false;
+        for(int i=0; i<arr.size(); i++){
+            if(arr[i] != target[i]){
                 return false;
             }
         }
         return true;
+        
+        // unordered_map<int, int>mp;
+        // for(auto it: target){
+        //     mp[it]++;
+        // }
+        // for(auto it: arr){
+        //     mp[it]--;
+        // }
+        // for(auto it: mp){
+        //     if(it.second != 0){
+        //         return false;
+        //     }
+        // }
+        // return true;
     }
 };
